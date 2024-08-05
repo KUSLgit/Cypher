a79ec5a7c44b8d13847a0cf1b19f86a893fc89d4
Kushal <129323774+KUSLgit@users.noreply.github.com>
2024-08-05 19:55:27 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,17 +1,24 @@
 
-var inputs = document.querySelectorAll('input');
-var element = document.querySelector('h1');
-
+var encryptText = document.getElementById('encrypt');
+var decryptText = document.getElementById('decrypt');
 async function copyToClipboard(selector) {
     var element = document.querySelector(selector);
     await navigator.clipboard.writeText(element.value);
 }
 
+var alphabet = 
 function encrypt() {
     var inp1 = document.querySelector('#inp1');
-    
-}
+    var letters = inp1.value.split('');
 
+    encryptText.innerHTML = "Encrypted: " + letters;
+    console.log(letters);
+
+}
+// encrypt();
 function decrypt() {
     var inp2 = document.querySelector('#inp2');
-}
+    var letters = inp2.value.split('');
+    decryptText.innerHTML = letters;
+    console.log(letters);
+} 
